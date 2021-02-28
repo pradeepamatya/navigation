661c5b7aeb079209257a10832b5bed2a19df6dfc
pradeepamatya <pradeepbdramatya@gmail.com>
2021-02-28 23:25:55 +0545

    font

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "ubuntu-bold": require("./assets/fonts/Ubuntu-Bold.ttf"),
+    ubuntu: require("./assets/fonts/Ubuntu-Regular.ttf"),
+  });
+};
 
 export default function App() {
+  const [isReady, setIsReady] = React.useState(false);
+
+  if (!isReady)
+    return (
+      <AppLoading
+        onError={(err) => console.log(err)}
+        startAsync={fetchFonts}
+        onFinish={() => setIsReady(true)}
+      />
+    );
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ margin: 50 }}>
+      <Text>Hello world</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
